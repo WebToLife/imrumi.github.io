e758563ec89a75d64ff3c8414cafb450e0d07d0f
rumi622 <rumi.projects100@gmail.com>
2024-06-14 10:29:02 +0530

    Add files via upload

---
+++ testproject.js
@@ -0,0 +1,39 @@
+
+
+let answer1=prompt("kahana kha lie?");
+console.log(answer1);
+
+let answer2=prompt("to fir kya khaye the?");
+console.log(answer2);
+
+let answer3=prompt("how was your day?");
+console.log(answer3);
+
+let answer4=prompt("ye mera first project hai,...thoda sa web development sikhe.. dumb project dikhrha hoga,web banane se jyada upload krne me wkt lg gya!! ")
+console.log(answer4);
+
+let answer5=prompt("Kam ki bat pr ate hai ,dekho!jyada gussa theek nhi hai, bta de rhe  hai!! ");
+console.log(answer5);
+
+let answer6=prompt("is lie ,fatafat se mafinama accept karo ");
+console.log(answer6);
+
+let answer7=prompt("so what you have desided?");
+console.log(answer7);
+
+let answer8=prompt("i am sorry,and sorry to tell you that ,this is an end of the promt.bye for now!!");
+console.log(answer8);
+
+ 
+
+
+
+
+let btn1=document.querySelector("#btn1");
+ btn1.onclick=() =>{
+   console.log("awesome!!"); 
+} 
+ let btn2=document.querySelector("#btn2")
+ btn1.onclick=() =>{
+    console,log("ya good but not really")
+ }
